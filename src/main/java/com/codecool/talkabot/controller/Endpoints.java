8135372071e1d1c961da3609251ac4ce499e7b9b
davidlenner <david910128@gmail.com>
2019-04-08 15:31:08 +0200

    send data fro mdb to endpoint

--- src/main/java/com/codecool/talkabot/controller/Endpoints.java
+++ src/main/java/com/codecool/talkabot/controller/Endpoints.java
@@ -1,4 +1,18 @@
 package com.codecool.talkabot.controller;
 
+import com.codecool.talkabot.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Endpoints {
+
+    @Autowired
+    HelloWorldService service;
+
+    @GetMapping("/helloworld")
+    public String getMessage() {
+        return service.getHelloWorld().get(0).getMessage();
+    }
 }
